2a27d4245b61be24c6e7e1a3e0238bbd4570b343
Rithboss <rithvikvanga@gmail.com>
2026-01-07 01:31:19 -0800

    x

--- backend/src/services/embedding/EmbeddingService.ts
+++
@@ -1,7 +0,0 @@
-// Stub for EmbeddingService - to be implemented
-export class EmbeddingService {
-  static async getEmbedding(text: string): Promise<number[]> {
-    // TODO: Implement actual embedding generation
-    throw new Error('Embedding service not yet implemented');
-  }
-}
